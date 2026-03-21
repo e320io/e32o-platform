ba6b1e552411e54a6556626cd0076bea500509ee
e320io <theecochicmx@gmail.com>
2026-03-20 23:45:27 -0600

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-export const STORAGE_URL = `${supabaseUrl}/storage/v1/object/public/creativos`;
+
+const SUPABASE_URL = 'https://fyiukqelspqdvdulczrs.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5aXVrcWVsc3BxZHZkdWxjenJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4NzUwMDAsImV4cCI6MjA4OTQ1MTAwMH0.HsZdGoLGCIbKcv3ytWTyjYrWeQ5yRJsp7O1Cj9lWO3g';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
